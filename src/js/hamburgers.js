3ca226f0c4a81199eee18168cbdcd62e9c1771e1
ulugbekwebp <ulugbekwebp@gmail.com>
2021-05-02 21:02:14 +0500

    new

---
+++ src/js/hamburgers.js
@@ -0,0 +1,9 @@
+var burger = document.querySelector(".header__hamburger");
+var nav = document.querySelector(".header__nav");
+// var scotin = document.querySelector(".header__scootin-link")
+
+burger.addEventListener("click", function(){
+  nav.classList.toggle('header__nav--active');
+  // scotin.classList.toggle('header__scootin-link--active');
+  burger.classList.toggle('header__hamburger--active')
+})
